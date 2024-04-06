630b849180c41b9fad5beaa2b7cd7a30955fa88d
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-04-06 23:16:47 +0530

    changes

--- 05_Password Generator/src/App.jsx
+++ 05_Password Generator/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
+import { useState,useCallback } from 'react'
 
 import './App.css'
 
 function App() {
   const [length, setLength] = useState(8)
   const [numberAllowed , SetnumberAllowed] = useState(false);
-  const [ character , setCharacter] = useState(false)
+  const [ characterAllowed , setCharacter] = useState(false);
+  const [password , setPassword] = useState("")
+  const passwordGenerator = useCallback(()=>{
+  let pass = ""
+  let str = ""
+  if (numberAllowed) str += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  if (characterAllowed) str += "!@#$%^&*(){}[]_,"
+  }, [length,numberAllowed,characterAllowed,setPassword])
+  for(let i =1 ; i <= array.length ; i++ ){
+    let char = Math.floor(Math.random() * str.length +1)
+    pass = str.charAt(char)
+    setPassword(pass)
+  }
 
   return (
     <>
-     <h1 className='text-4xl text-center text-white'>Password Generator</h1>
-    </>
+    <div className='w-full max-w-md m-auto shadow-md rounded=lg px-4 my-8 text-orange-500 bg-gray-300'> 
+      </div>    </>
   )
 }
 
 export default App
+
