e60d1f2076f34269cd0f1f48a23ca991f6b44552
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-04-27 15:38:52 +0530

    card making

--- 00-mini docs app/src/App.jsx
+++ 00-mini docs app/src/App.jsx
@@ -2,33 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Background from './components/Background'
+import Foreground from './components/Foreground'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App() {  
+ 
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+<div className='relative w-full h-screen bg-zinc-800 m-0 p-0'> 
+<Background/>
+<Foreground />
+</div>
+   
   )
 }
 
---
+++ 00-mini docs app/src/components/Foreground.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Foreground() {
+  return (
+    <><div className='fixed top-0 left-0 z-[3] w-full h-full '></div>
+    </>
+  )
+}
+
+export default Foreground
