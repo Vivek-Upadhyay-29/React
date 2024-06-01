6d173ab9706ac3c3fae9f98356f5225db162b5b8
Vivek_Upadhyay_29 <uvivek2952003@gmail.com>
2024-06-01 21:07:35 +0530

    commit

--- 04Practice Project/src/App.jsx
+++ 04Practice Project/src/App.jsx
@@ -23,17 +23,23 @@ function App() {
       <h1 className="h1tag" > Counter {counter} </h1>
     <button  className="button"  onClick={addValue}> Add Value </button>
     <button className= "button " onClick={reduceValue}> Reduce Value </button>
+
     <button className="button bg-blue-500 text-white px-4 py-2" onClick={()=>{
       setColor("green")
     }} style={{backgroundColor:"green"}} >  Green </button>
+
+
      <button className="button bg-blue-500 text-white px-4 py-2" onClick={()=>{
       setColor("pink")
     }} style={{backgroundColor:"pink"}} >  pink </button>
+
+
      <button className="button bg-blue-500 text-white px-4 py-2 hover:bg-blue-700 focus:outline-none" onClick={()=>{
       setColor("yellow")
     }} style={{backgroundColor:"yellow"}} >  yellow </button>
 
-<button  class ="my-custom-button" className="button bg-blue-500 text-white px-4 py-2" onClick={()=>{
+
+    <button  class ="my-custom-button" className="button bg-blue-500 text-white px-4 py-2" onClick={()=>{
       setColor("orange")
     }} style={{backgroundColor:"orange"}} >  orange </button>
 
